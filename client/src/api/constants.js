9f73d2d1e53d8459141249e6bec31dd36cd31496
devakhil-kumar <akhil.cyberbells.com>
2024-05-23 07:21:11 -0700

    changes api

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,4 +1,4 @@
 export const API_ROUTES = {
-    FETCH_DATA: 'tmb-count',
+    FETCH_DATA: 'tmb-count?field=carrier&value=Telstra Mobile Broadband (Consumer)',
     // Add more routes here as needed
   };
