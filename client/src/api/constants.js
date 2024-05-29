00160022b903793a4b052d9b8041142e0182a559
devakhil-kumar <akhil.cyberbells.com>
2024-05-28 21:58:28 -0700

    server

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,6 +1,7 @@
 export const API_ROUTES = {
     // FETCH_DATA: 'tmb-count?field=carrier&value=Telstra Mobile Broadband (Consumer)',
     FETCH_DATA:'data-by-store',
+    GET_ALL:'get-all',
     CREATE_TARGET: '/target',
   GET_TARGET: 'target',
   UPDATE_TARGET: '/target',
--- server/server.js
+++ server/server.js
@@ -15,8 +15,7 @@ app.use(express.json());
 // app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://135.181.192.4:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
   next();
 });
 
