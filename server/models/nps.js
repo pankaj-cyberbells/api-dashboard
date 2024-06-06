392c2ca07ef75831eec2a8e7d1859aff6208b304
Janagokul6 <janagokul6@gmail.com>
2024-06-06 18:24:05 +0530

    duplicate entry removed

--- server/models/nps.js
+++ server/models/nps.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const npsSchema = mongoose.Schema(
   {
-    salesrep: { type: String },
-    salelocation: { type: String },
+    salesrep: { type: String,  required: true  },
+    salelocation: { type: String,  required: true  },
     NPSVol: { type: Number },
     NPSScore: { type: Number },
     adv10_9: { type: Number },
     pass8_7: { type: Number },
     detr_less_6: { type: Number },
-    updatedBy: { type: String },
+    updatedBy: { type: String ,  required: true },
   },
   { timestamps: true }
 );
