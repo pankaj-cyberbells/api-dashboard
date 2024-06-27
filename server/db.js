c1aa3d8b5a2c478cd35c9362730f5d6bc7a24ad7
devakhil-kumar <akhil.cyberbells.com>
2024-06-27 03:38:44 -0700

    changes

--- server/db.js
+++ server/db.js
@@ -3,13 +3,13 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 const connectionURL = process.env.MONGO_URI
-
+console.log(connectionURL,"jj")
 export const connectToDB = async() => {
 
   mongoose.connect(connectionURL, {}).then(() => {
     console.log("db connected successfully")
   }).catch((err) => {
-    console.log(err)
+    console.log(err,"hh")
   })
 
 }
