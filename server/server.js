58e55bdfb17c6c73ce9e6b3cfd490de6cc3eda44
devakhil-kumar <akhil.cyberbells.com>
2024-06-26 21:50:59 -0700

    cors

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ const app=express();
 
 
 app.use(express.json());
-app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://135.181.192.4:3000');
-// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-// res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://135.181.192.4:3000');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get("/", (req,res)=>{
   res.send("<h1>hello from express server </h1>")
